eaa4f0ad65148ba01bc636bd99dae955a321da2e
Denis Kropp <Leichenbegatter@outlook.com>
2022-10-30 07:20:47 +0000

    initial ./php

---
+++ php/api/Teilnehmer.php
@@ -0,0 +1,34 @@
+<?php 
+	include '../cors.php';
+
+	$conn = require '../db.php';
+
+	$sql = 'SELECT * FROM Teilnehmer';
+
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+
+		// write query for all users
+		$sql .= ' WHERE id = '.$id;
+	}
+	else {
+		// write query for all users
+		$sql .= ' ORDER BY Name';
+	}
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$entries = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+?>
+
+<?php
+	echo json_encode($entries)
+?>
---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<title>WeTeK Berlin gGmbH - Lade...</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta http-equiv="refresh" content="0; http://a.violass.club:8000/">
+</head>
+
+<body>
+</body>
+
+</html>
