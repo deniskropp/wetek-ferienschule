904aca782721e05d9a7e07a9617b3059cd543b52
Denis Kropp <Leichenbegatter@outlook.com>
2022-11-18 21:29:30 +0000

    v3

--- db/doltdump.sql
+++ db/doltdump.sql
@@ -12,10 +12,25 @@ INSERT INTO `Teilnehmer` (`id`,`Name`,`Vorname`,`Klasse`,`Email`) VALUES (1,'Kro
 INSERT INTO `Teilnehmer` (`id`,`Name`,`Vorname`,`Klasse`,`Email`) VALUES (2,'Müller','Viola','9c','violamueller@gmail.com');
 INSERT INTO `Teilnehmer` (`id`,`Name`,`Vorname`,`Klasse`,`Email`) VALUES (3,'Schmidt','Jens','10a','schmidtj@gmail.com');
 INSERT INTO `Teilnehmer` (`id`,`Name`,`Vorname`,`Klasse`,`Email`) VALUES (4,'Scholz','Olaf','1a','olaf.scholz@bundestag.com');
-DROP TABLE IF EXISTS `Teilnahme`;
-CREATE TABLE `Teilnahme` (
+
+DROP TABLE IF EXISTS `Klassen`;
+CREATE TABLE `Klassen` (
+  `id` int NOT NULL,
+  `Name` text NOT NULL,
+  `Schule` text NOT NULL,
+  `dateModified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `Anwesenheit`;
+CREATE TABLE `Anwesenheit` (
   `Teilnehmer_id` int NOT NULL,
+  `Klassen_id` int NOT NULL,
   `Datum` timestamp NOT NULL,
   `dateModified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Teilnehmer_id`, `Datum`)
+  PRIMARY KEY (`Teilnehmer_id`, `Klassen_id`, `Datum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `Anwesenheit` (`Teilnehmer_id`,`Klassen_id`,`Datum`) VALUES (1,0,'2022-11-14 07:27:33');
+INSERT INTO `Anwesenheit` (`Teilnehmer_id`,`Klassen_id`,`Datum`) VALUES (1,0,'2022-11-13 08:34:10');
+INSERT INTO `Anwesenheit` (`Teilnehmer_id`,`Klassen_id`,`Datum`) VALUES (1,0,'2022-11-12 10:13:20');
+INSERT INTO `Anwesenheit` (`Teilnehmer_id`,`Klassen_id`,`Datum`) VALUES (1,0,'2022-11-11 09:11:06');
