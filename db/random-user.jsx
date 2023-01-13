33e979e8057758f956bdae0e9f8d74977d51c9c8
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-01-14 00:39:51 +0100

    switch to random-user by api-ninjas

--- db/random-user.jsx
+++ db/random-user.jsx
@@ -2,15 +2,18 @@ const axios = require("axios");
 
 const options = {
   method: 'GET',
-  url: 'https://random-user.p.rapidapi.com/getuser',
+  url: 'https://random-user-by-api-ninjas.p.rapidapi.com/v1/randomuser',
   headers: {
-    'X-RapidAPI-Key': 'acee344339mshfa967de0b6a29d8p148e75jsn9a5790c07991',
-    'X-RapidAPI-Host': 'random-user.p.rapidapi.com'
+    'X-RapidAPI-Key': '185666417cmshcb6cb46fcb0fe21p1f568djsnf53e92f4ca97',
+    'X-RapidAPI-Host': 'random-user-by-api-ninjas.p.rapidapi.com'
   }
 };
 
-axios.request(options).then(function (response) {
-	console.log(`(Name,Vorname,Email,Klasse) VALUES ('${response.data.results[0].name.last}','${response.data.results[0].name.first}','${response.data.results[0].email}',0)`);
-}).catch(function (error) {
-	console.error(error);
-});
+async function load() {
+	const response = await axios.request(options)
+	const [Vorname, Name] = response.data.name.split(' ')
+
+	console.log(`(Name,Vorname,Email,Klassen_id) VALUES ('${Name}','${Vorname}','${response.data.email}',1)`);
+}
+
+load()
