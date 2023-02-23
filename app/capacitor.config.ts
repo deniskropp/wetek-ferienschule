e84fa205edfa11a25602cfeb1596727220b2ac56
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-02-23 15:08:42 +0100

    android

---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.wetek',
+  appName: 'ferienschule-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
